1541d9c6f9b8d6f28fada9c23f3e0bd2039e4312
Abdallah <tarqb131@gmail.com>
2026-08-26 17:20:45 +0300

    Refactor MyApp to use BlocBuilder for weather state management and remove CustomMatrialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:weather_app/logic/get_matrialcolor_of_conditional_day.dart';
 import 'package:weather_app/pages/home_view.dart';
 
@@ -16,22 +17,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => GetWeatherCubit(),
-      child: CustomMatrialApp(),
-    );
-  }
-}
-
-class CustomMatrialApp extends StatelessWidget {
-  const CustomMatrialApp({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: getWeatherColor(BlocProvider.of<GetWeatherCubit>(context).weatherModel?.weatherCondition)),
-      home: HomeScreen(),
+      child: Builder(
+        builder: (context) {
+          return BlocBuilder<GetWeatherCubit, WeatherStates>(
+            builder: (context, state) {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                  primarySwatch: getWeatherColor(
+                    BlocProvider.of<GetWeatherCubit>(
+                      context,
+                    ).weatherModel?.weatherCondition,
+                  ),
+                ),
+                home: HomeScreen(),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
