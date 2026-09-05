cd77edef265164eee7fde6d70774adc3f3fd9ab1
Abdallah <tarqb131@gmail.com>
2026-09-05 22:02:58 +0300

    Refactor WeatherInfo widget to enhance UI with gradient background and display weather image and temperature details

--- lib/widgets/weather_info.dart
+++ lib/widgets/weather_info.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
 import 'package:weather_app/data/model/weather_model.dart';
+import 'package:weather_app/logic/get_matrialcolor_of_conditional_day.dart';
 
 class WeatherInfo extends StatelessWidget {
   const WeatherInfo({super.key});
@@ -11,54 +12,67 @@ class WeatherInfo extends StatelessWidget {
     WeatherModel weatherModel = BlocProvider.of<GetWeatherCubit>(
       context,
     ).weatherModel!;
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            weatherModel.cityName,
-            style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            "Updata at:${weatherModel.date.hour}:${weatherModel.date.minute}",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20, bottom: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.network("https:${weatherModel.image}"),
-                Text(
-                  weatherModel.temp.toString(),
-                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-                ),
-                Column(
-                  children: [
-                    Text(
-                      'max temp: ${weatherModel.maxTemp.round()}',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Text(
-                      'min temp: ${weatherModel.minTemp.round()}',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            getWeatherColor(weatherModel.weatherCondition),
+            getWeatherColor(weatherModel.weatherCondition)[300]!,
+            getWeatherColor(weatherModel.weatherCondition)[50]!,
+          ],
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              weatherModel.cityName,
+              style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
             ),
-          ),
-          Text(
-            weatherModel.weatherCondition,
-            style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-          ),
-        ],
+            Text(
+              "Updata at:${weatherModel.date.hour}:${weatherModel.date.minute}",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.network("https:${weatherModel.image}"),
+                  Text(
+                    weatherModel.temp.toString(),
+                    style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'max temp: ${weatherModel.maxTemp.round()}',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'min temp: ${weatherModel.minTemp.round()}',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              weatherModel.weatherCondition,
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
